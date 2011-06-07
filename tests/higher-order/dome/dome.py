4c3c64a4c26eb316fdfe60abdb0085e3d10affed
sprice <sprice@lanl.gov>
2011-06-07 19:50:57 +0000

    Minor changes to .py script and readme to allow for parallel runs.
    git-svn-id: https://svn-cism-model.cgd.ucar.edu/glimmer-cism2/branches/glimmer-cism-lanl@1074 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- tests/higher-order/dome/dome.py
+++ tests/higher-order/dome/dome.py
@@ -87,9 +87,10 @@ netCDFfile.close()
 # Run Glimmer
 print 'Running Glimmer/CISM'
 if len(sys.argv) > 2:
-   os.system('aprun -n'+nprocs+' ./simple_glide '+configfile+'')  # support for MPI runs is here
+   #os.system('aprun -n'+nprocs+' ./simple_glide '+configfile+'')  # support for MPI runs is here
+   os.system('mpirun -np '+nprocs+' ./simple_glide '+configfile+'') 
 else:
-   os.system('echo '+configfile+' | simple_glide')
+   os.system('echo '+configfile+' | ./simple_glide')
 
 # Clean up by moving extra files written by Glimmer to the "scratch" subdirectory
 # Look for files with extension "txt", "log", or "nc"
