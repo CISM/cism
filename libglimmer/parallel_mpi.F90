8ec8b5b765b2fddd17753b9a0866495e09ba1c4a
evanskj@ornl.gov <evanskj@ornl.gov>
2012-08-20 19:48:37 +0000

    revert parallel_slap to pre-halo adjustment. Also update gnu script for jaguar to a version built in new software environment
    git-svn-id: https://svn-cism-model.cgd.ucar.edu/glimmer-cism2/branches/seacism@1420 2cfc7cc4-626f-0410-8325-f6cb4dace9bf
    
    Former-commit-id: 39ae7a591474fdc712f23cb23a5ce5d6a26109d1

--- libglimmer/parallel_mpi.F90
+++ libglimmer/parallel_mpi.F90
@@ -2685,7 +2685,8 @@ contains
 
     ! unknown grid
     if (size(a,2)/=local_ewn.or.size(a,3)/=local_nsn) then
-         write(*,*) "Unknown Grid: Size a=(", size(a,1), ",", size(a,2), ",", size(a,3), ") and local_ewn and local_nsn = ", local_ewn, ",", local_nsn
+         write(*,*) "Unknown Grid: Size a=(", size(a,1), ",", size(a,2), ",", size(a,3), ") &
+                 and local_ewn and local_nsn = ", local_ewn, ",", local_nsn
          call parallel_stop(__FILE__,__LINE__)
     endif
 
