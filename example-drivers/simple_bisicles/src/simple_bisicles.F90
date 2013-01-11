a06b2c2f88d16b2013b1f4ee9b473f8ddac8f2dd
ranken@lanl.gov <ranken@lanl.gov>
2013-01-11 00:26:41 +0000

    Removed a lot of unneeded code.  Working towards a version that will run in MPI mode while using more than 1 process.
    git-svn-id: https://svn-cism-model.cgd.ucar.edu/glimmer-cism2/branches/seacism@1685 2cfc7cc4-626f-0410-8325-f6cb4dace9bf
    
    Former-commit-id: 0fb02196ca136447c3dcd9a86ad34297f2d69ecd

--- example-drivers/simple_bisicles/src/simple_bisicles.F90
+++ example-drivers/simple_bisicles/src/simple_bisicles.F90
@@ -34,9 +34,13 @@
 program simple_bisicles
   !*FD This is a simple GLIDE test driver. It can be used to run
   !*FD the EISMINT test cases
+
+!  use parallel, only: nsub, ewub, nslb, ewlb, global_nsn, global_ewn, own_ewn, own_nsn, lhalo, uhalo, &
+!                        rank => this_rank, ewtasks => ProcsEW, tasks, comm
   use parallel
   use glimmer_global, only:rk
   use glide
+  use glissade
   use simple_forcing
   use glimmer_log
   use glimmer_config
@@ -52,12 +56,6 @@ program simple_bisicles
 
   implicit none
 
-#ifdef GPTL
-#include <gptl.inc>
-#endif
-#ifdef PAPI
-#include <f90papi.h>
-#endif
 
   type(glide_global_type) :: model        ! model instance
   type(simple_climate) :: climate         ! climate
@@ -73,25 +71,14 @@ program simple_bisicles
   ! for external dycore:
   integer*4 dycore_model_index
   integer argc
-print *,"Calling parallel_initialize "
-  call parallel_initialise
-print *,"Returned."
 
-  ! start gptl
-#ifdef GPTL
-  ret = gptlsetoption (gptlprint_method,gptlfull_tree)
-  ret = gptlsetoption (PAPI_FP_OPS, 1)
-  ret = gptlsetutr (gptlnanotime)
-  ret = gptlinitialize ()
-  ret = gptlstart ('total')
-#endif
+  call parallel_initialise
 
   call glimmer_GetCommandline()
 
-
   ! start logging
   call open_log(unit=50, fname=logname(commandline_configname))
-  
+
   ! setup paths
   call filenames_init(commandline_configname)
 
@@ -104,8 +91,12 @@ print *,"Returned."
 
   ! initialise GLIDE
   call glide_config(model,config)
-  call simple_initialise(climate,config)
-  call glide_initialise(model)
+!  call simple_initialise(climate,config)
+
+  ! replace glide_initialise with glissade_initialise,
+  ! for consistent halos in serial mode:
+  call glissade_initialise(model)
+
   call CheckSections(config)
   ! fill dimension variables
   call glide_nc_fillall(model)
@@ -115,112 +106,20 @@ print *,"Returned."
   call simple_massbalance(climate,model,time)
   call simple_surftemp(climate,model,time)
   call spinup_lithot(model)
-print *,"Calling gtd_init_dycore_interface "
+
+! if (this_rank == 0) then
+
+  print *,"Initializing external dycore interface."
   call gtd_init_dycore_interface()
-print *,"Calling gtd_init_dycore "
+
+  print *,"Initializing external dycore."
   call gtd_init_dycore(model,dycore_model_index)
-print *,"Returned."
-print *,"Calling gtd_run_dycore "
+
+  print *,"Running external dycore."
   call gtd_run_dycore(dycore_model_index,cur_time,time_inc)
-print *,"Returned."
+  print *,"Completed Dycore Run."
 
-  tstep_count = 0
-print *,"Entering while loop"
-  do while(time.le.model%numerics%tend)
-     call glide_tstep_p1(model,time)
-     call glide_tstep_p2(model)
-     call glide_tstep_p3(model)
-     ! override masking stuff for now
-
-     tstep_count = tstep_count + 1
-
-     ! Redistribute calls here to spread the data back out.
-     call parallel_halo(model%stress%efvs)
-     call horiz_bcs_unstag_scalar(model%stress%efvs)
-     call parallel_halo(model%velocity%uvel)
-     call horiz_bcs_stag_vector_ew(model%velocity%uvel)
-     call parallel_halo(model%velocity%vvel)
-     call horiz_bcs_stag_vector_ns(model%velocity%vvel)
-     call parallel_halo(model%velocity%uflx)
-     call horiz_bcs_stag_vector_ew(model%velocity%uflx)
-     call parallel_halo(model%velocity%vflx)
-     call horiz_bcs_stag_vector_ns(model%velocity%vflx)
-     call parallel_halo(model%velocity%velnorm)
-     call horiz_bcs_stag_scalar(model%velocity%velnorm)
-     call parallel_halo(model%geometry%thck)
-     call horiz_bcs_unstag_scalar(model%geometry%thck)
-     call parallel_halo(model%geomderv%stagthck)
-     call horiz_bcs_stag_scalar(model%geomderv%stagthck)
-     call parallel_halo(model%climate%acab)
-     call horiz_bcs_unstag_scalar(model%climate%acab)
-     call parallel_halo(model%geomderv%dusrfdew)
-     call horiz_bcs_stag_vector_ew(model%geomderv%dusrfdew)
-     call parallel_halo(model%geomderv%dusrfdns)
-     call horiz_bcs_stag_vector_ns(model%geomderv%dusrfdns)
-     call parallel_halo(model%geomderv%dthckdew)
-     call horiz_bcs_stag_vector_ew(model%geomderv%dthckdew)
-     call parallel_halo(model%geomderv%dthckdns)
-     call horiz_bcs_stag_vector_ns(model%geomderv%dthckdns)
-     call parallel_halo(model%stress%tau%xx)
-     call horiz_bcs_unstag_scalar(model%stress%tau%xx)
-     call parallel_halo(model%stress%tau%yy)
-     call horiz_bcs_unstag_scalar(model%stress%tau%yy)
-     call parallel_halo(model%stress%tau%xy)
-     call horiz_bcs_unstag_scalar(model%stress%tau%xy)
-     call parallel_halo(model%stress%tau%scalar)
-     call horiz_bcs_unstag_scalar(model%stress%tau%scalar)
-     call parallel_halo(model%stress%tau%xz)
-     call horiz_bcs_unstag_scalar(model%stress%tau%xz)
-     call parallel_halo(model%stress%tau%yz)
-     call horiz_bcs_unstag_scalar(model%stress%tau%yz)
-     call parallel_halo(model%geometry%topg)
-     call horiz_bcs_unstag_scalar(model%geometry%topg)
-     call parallel_halo(model%geometry%thkmask)
-     call horiz_bcs_unstag_scalar(model%geometry%thkmask)
-     call parallel_halo(model%geometry%marine_bc_normal)
-     call horiz_bcs_unstag_scalar(model%geometry%marine_bc_normal)
-     call parallel_halo(model%velocity%surfvel)
-     call horiz_bcs_stag_scalar(model%velocity%surfvel)
-     call parallel_halo(model%ground%gline_flux)
-     call horiz_bcs_unstag_scalar(model%ground%gline_flux)
-     call parallel_halo(model%velocity%ubas)
-     call horiz_bcs_stag_vector_ew(model%velocity%ubas)
-     call parallel_halo(model%velocity%vbas)
-     call horiz_bcs_stag_vector_ns(model%velocity%vbas)
-     call parallel_halo(model%isos%relx)
-     call horiz_bcs_unstag_scalar(model%isos%relx)
-     call parallel_halo(model%temper%flwa)
-     call horiz_bcs_unstag_scalar(model%temper%flwa)
-     call parallel_halo(model%climate%calving)
-     call horiz_bcs_unstag_scalar(model%climate%calving)
-     call parallel_halo(model%climate%backstress)
-     call horiz_bcs_unstag_scalar(model%climate%backstress)
-     call parallel_halo(model%geometry%usrf)
-     call horiz_bcs_unstag_scalar(model%geometry%usrf)
-     call parallel_halo(model%climate%backstressmap)
-     call horiz_bcs_unstag_scalar(model%climate%backstressmap)
-     call parallel_halo(model%stress%tau_x)
-     call horiz_bcs_stag_vector_ew(model%stress%tau_x)
-     call parallel_halo(model%stress%tau_y)
-     call horiz_bcs_stag_vector_ew(model%stress%tau_y)
-     call parallel_halo(model%geometry%lsrf)
-     call horiz_bcs_unstag_scalar(model%geometry%lsrf)
-
-     if (model%options%whichtemp == TEMP_REMAP_ADV) then
-        call parallel_halo(model%temper%temp)
-        call horiz_bcs_unstag_scalar(model%temper%temp)
-     else
-        call parallel_halo_temperature(model%temper%temp)
-        call horiz_bcs_unstag_scalar(model%temper%temp)
-     endif
-
-     ! Perform parallel operations for restart files
-     call glide_tstep_postp3(model)
-
-     time = time + model%numerics%tinc
-     call simple_massbalance(climate,model,time)
-     call simple_surftemp(climate,model,time)     
-  end do
+! endif
 
   ! finalise GLIDE
   call glide_finalise(model)
@@ -228,13 +127,7 @@ print *,"Entering while loop"
   t2 = real(clock,kind=dp)/real(clock_rate,kind=dp)
   call glimmer_write_stats(commandline_resultsname,commandline_configname,t2-t1)
   call close_log
-  call parallel_finalise
 
-  ! stop gptl
-#ifdef GPTL
-  ret = gptlstop ('total')
-  ret = gptlpr (0)
-  ret = gptlfinalize ()
-#endif
+  call parallel_finalise
   
 end program simple_bisicles
