a2a353e2f3d781877456ef504b60f5851553c384
sacks <sacks@ucar.edu>
2013-05-01 15:57:38 +0000

    adding LGPL headers to libglimmer-trilinos source files, based on input from Andy Salinger
    git-svn-id: https://svn-cism-model.cgd.ucar.edu/glimmer-cism2/branches/seacism@1853 2cfc7cc4-626f-0410-8325-f6cb4dace9bf
    
    Former-commit-id: cd4b5cba07cc2d74bf2a8020bbcd3e68fb3b4246

--- libglimmer-trilinos/trilinosNoxSolver.cpp
+++ libglimmer-trilinos/trilinosNoxSolver.cpp
@@ -1,3 +1,29 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                                             
+//   trilinosNoxSolver.cpp - part of the Glimmer Community Ice Sheet Model (Glimmer-CISM)  
+//                                                              
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+//   Copyright (C) 2005-2013
+//   Glimmer-CISM contributors - see AUTHORS file for list of contributors
+//
+//   This file is part of Glimmer-CISM.
+//
+//   Glimmer-CISM is free software: you can redistribute it and/or modify it
+//   under the terms of the Lesser GNU General Public License as published
+//   by the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   Glimmer-CISM is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   Lesser GNU General Public License for more details.
+//
+//   You should have received a copy of the Lesser GNU General Public License
+//   along with Glimmer-CISM. If not, see <http://www.gnu.org/licenses/>.
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 // Trilinos Objects
 #include "Piro_Epetra_NOXSolver.hpp"
 #include "Piro_Epetra_LOCASolver.hpp"
