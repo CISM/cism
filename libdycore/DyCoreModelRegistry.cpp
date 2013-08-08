4c3f467e8b7c2caa101329e61189064d1e190b18
ikalash <ikalash@sandia.gov>
2013-08-08 19:14:49 +0000

    Adding new source code to allow creation of simple_felix executable that would allow linking CISM to Albany/FELIX through libdycore interface.
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@585 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: f528fe7612d484e55b160ba83cd4cd0ced202344

--- libdycore/DyCoreModelRegistry.cpp
+++ libdycore/DyCoreModelRegistry.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib>
 #include "DyCoreModelRegistry.H"
 
 using namespace std;
@@ -52,6 +53,13 @@ DyCoreModelRegistry::getDyCoreToGlimmerByIndex(int index)
 {
   
   // cout << index << " Registry entry dycore type: " << entry[index].dycore_type << endl;
+  //IK, 8/6/13: added error checking here so that if entry[index].dycore_to_glimmer is has not been set 
+  //properly in setDyCoreByType, the code will abort.    
+  if ((entry[index].dycore_to_glimmer) == NULL) {
+    fprintf(stderr, "Error in DyCoreModelRegistry::getDyCoreToGlimmerByIndex: external dycore not set properly in setDyCoreByType. \n");
+    fprintf(stderr, "Aborting... \n");
+    exit(1); 
+  }
   return((DyCoreToGlimmer *) entry[index].dycore_to_glimmer);
 }
 
@@ -62,13 +70,27 @@ DyCoreModelRegistry::setDyCoreByType(int index,int dycore_type)
   entry[index].dycore_present = 1;
 
   switch (entry[index].dycore_type) {
-    case 0: 
+    case 0:
+//IK, 8/6/13: added ifdefs here to allow multiple external dycores
+#ifdef CISM_HAS_BISICLES
        entry[index].dycore_to_glimmer = new BisiclesToGlimmer;
+#else 
+       entry[index].dycore_to_glimmer = NULL;
+#endif
        break;
     case 1:
       //entry[index].dycore_to_glimmer = new YmirToGlimmer;
        break;
-    default: entry[index].dycore_to_glimmer = new BisiclesToGlimmer;
+//IK, 8/6/13: added case for FELIX dycore 
+    case 2: 
+#ifdef CISM_HAS_FELIX
+       entry[index].dycore_to_glimmer = new FelixToGlimmer;
+#else 
+       entry[index].dycore_to_glimmer = NULL;
+#endif
+       break;
+    default: entry[index].dycore_to_glimmer = NULL;
+       break; 
   }
   return(0);
 }
