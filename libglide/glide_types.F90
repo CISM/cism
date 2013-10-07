cc04ba8d229eb0fec415c2a3076848b5cc1bbfd0
mhoffman <mhoffman@lanl.gov>
2013-10-07 20:09:29 +0000

    Committing a few lines of the isostasy option fix that I inadvertently left out earlier today.
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@714 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: f84d82f6ea26700568fa8e33591bcaab6883025f

--- libglide/glide_types.F90
+++ libglide/glide_types.F90
@@ -1444,10 +1444,10 @@ contains
     ! isostasy arrays
 
     call coordsystem_allocate(model%general%ice_grid, model%isostasy%relx)  ! MJH: relx needs to be allocated always.
-    !if (model%options%isostasy == ISOSTASY_COMPUTE) then
+    if (model%options%isostasy == ISOSTASY_COMPUTE) then
        call coordsystem_allocate(model%general%ice_grid, model%isostasy%load)
        call coordsystem_allocate(model%general%ice_grid, model%isostasy%load_factors)
-    !endif
+    endif
 
     !TODO - Are these needed?  Commented out for now
     ! phaml arrays
