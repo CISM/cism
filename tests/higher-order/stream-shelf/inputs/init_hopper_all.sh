56647f6329707b9541fe77ff5992867d3b441476
dfmartin <DFMartin@lbl.gov>
2013-10-21 06:45:14 +0000

    added script to generate inputs and qsub file to re-run BISICLES runs using existing melt rates
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@757 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: 335bf56fbfc7373099f3e733ed90c4cafdce4d3c

---
+++ tests/higher-order/stream-shelf/inputs/init_hopper_all.sh
@@ -0,0 +1,151 @@
+#!/bin/ksh
+
+#commonly changed parameters
+NEND=30 # number of iterations we're doing
+#RUNHOME=/scratch/users/dmartin/newXylar
+RUNHOME=.
+#directory containing input nc files
+
+
+
+INFILE_TEMPLATE=inputs.BISICLES.template
+CONFIGFILE_TEMPLATE=stream-shelf.config.template
+QSUBFILE_TEMPLATE=hopper.qsb.template
+
+
+#function to get dt
+getdt()
+{
+  case $STEPSPERYEAR in
+    12) DT=0.08333333333333333;;
+    24) DT=0.04166666666666667;;
+    3)  DT=0.33333333333333333;;
+    6)  DT=0.16666666666666667;;
+    *) echo "Unanticipated STEPSPERYEAR val for dt";;
+esac
+}
+
+for STEPSPERYEAR in 3 
+do
+echo "generating inputs for" $STEPSPERYEAR " steps per year"
+
+POPRESULTSDIR=popResults
+PERYEAR=_per_year
+DIR=$RUNHOME/$STEPSPERYEAR$PERYEAR/
+INFILE1_BASE=inputs.BISICLES.$STEPSPERYEAR$PERYEAR
+CONFIGFILE_BASE=stream-shelf.$STEPSPERYEAR$PERYEAR
+CONFIGSUFFIX=config
+QSBSUFFIX=qsb
+
+#INDIR=.
+#INDIR="\/global\/project\/projectdirs\/iceocean\/bisicles-pop\/XylarToDan\/$STEPSPERYEAR$PERYEAR"
+#INDIR="\/scratch\/scratchdirs\/xylar\/Goldberg\/coupling\/$STEPSPERYEAR$PERYEAR\/popResults"
+INDIR="\/scratch2\/scratchdirs\/dmartin\/Goldberg\/bisicles-rerun\/xylar\/$STEPSPERYEAR$PERYEAR"
+#directory containing output nc files
+#OUTDIR=.
+#OUTDIR="\/global\/project\/projectdirs\/iceocean\/bisicles-pop\/DanToXylar\/$STEPSPERYEAR$PERYEAR"
+OUTDIR="\/global\/project\/projectdirs\/iceocean\/bisicles-pop\/DanToXylar\/$STEPSPERYEAR$PERYEAR"
+
+
+echo $DIR
+mkdir -p $DIR
+
+ getdt   
+ echo "dt = " $DT
+
+ TSTART=0.0
+ TEND=$DT
+ 
+LASTSTEP=""
+NSTEP=0
+while [ $NSTEP -le $NEND ]
+do 
+#  echo $NSTEP
+  if [ $NSTEP -le 9 ]
+  then
+    STEP="0000"$NSTEP
+  elif [ $NSTEP -lt 99 ]
+  then 
+    STEP="000"$NSTEP
+  elif [ $NSTEP -lt 999 ]
+  then 
+    STEP="00"$NSTEP
+  elif [ $NSTEP -lt 9999 ]
+  then 
+    STEP="0"$NSTEP
+  else
+    STEP=$n
+  fi
+#  echo $STEP
+    
+#    TSTART=$(($STEP * $DT))
+#   echo $TSTART
+
+    of1=$DIR$INFILE1_BASE.$STEP
+    of2=$DIR$CONFIGFILE_BASE.$STEP.$CONFIGSUFFIX
+
+#    echo $of1
+#    echo $of2
+#    echo $of3
+#    echo $of4
+
+    sed  -e s/@STEPSPERYEAR/$STEPSPERYEAR/ -e s/@STEP/$STEP/ -e s/@LASTSTEP/$LASTSTEP/   $INFILE_TEMPLATE > $of1
+    sed  -e s/@STEPSPERYEAR/$STEPSPERYEAR/ -e s/@STEP/$STEP/ -e s/@LASTSTEP/$LASTSTEP/ -e s/@DT/$DT/ -e s/@TEND/$TEND/ -e s/@TSTART/$TSTART/ -e s/@INDIR/$INDIR/ -e s/@OUTDIR/$OUTDIR/  $CONFIGFILE_TEMPLATE > $of2
+
+    TSTART=$TEND
+    NSTEP=$((NSTEP + 1))
+    TEND=$((TEND + DT))
+    LASTSTEP=$STEP
+
+#    echo $NSTEP
+done
+
+done 
+
+#generate hopper qsub file
+
+echo "generating qsub file for hopper"
+QSUBFILE=BISICLES-all.$STEPSPERYEAR$PERYEAR.hopper
+
+#clean up pre-existing files
+rm -f $QSUBFILE
+
+echo "#PBS -q regular" >> $QSUBFILE
+echo "#PBS -l mppwidth=24" >> $QSUBFILE
+echo "#PBS -l walltime=6:00:00" >> $QSUBFILE
+echo "#PBS -N BISICLES-all-$STEPSPERYEAR$PERYEAR" >> $QSUBFILE
+echo "#PBS -V " >> $QSUBFILE
+echo "  " >> $QSUBFILE
+echo "cd \$PBS_O_WORKDIR/$STEPSPERYEAR$PERYEAR" >> $QSUBFILE
+
+LASTSTEP=""
+NSTEP=2
+
+while [ $NSTEP -le $NEND ]
+do 
+#  echo "step = "  $NSTEP
+  if [ $NSTEP -le 9 ]
+  then
+    STEP="0000"$NSTEP
+  elif [ $NSTEP -lt 99 ]
+  then 
+    STEP="000"$NSTEP
+  elif [ $NSTEP -lt 999 ]
+  then 
+    STEP="00"$NSTEP
+  elif [ $NSTEP -lt 9999 ]
+  then 
+    STEP="0"$NSTEP
+  else
+    STEP=$n
+  fi
+
+  echo "aprun -n 16 ../simple_bisicles stream-shelf.3_per_year.$STEP.config " >> $QSUBFILE
+
+    NSTEP=$((NSTEP + 1))
+
+done
+
+exit 0
+
+
