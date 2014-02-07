9cc9f6e4f5355a5ec0896c9c1b765a0ad8b29c12
mhoffman <mhoffman@lanl.gov>
2014-02-07 02:40:47 +0000

    Modernization of the dome.py script.
    Commanline options are parsed with the optParse module.  Script can be executed with --help for details of allowable options.
    If a parallel run is requested the script tries to determine which of 'openmpirun', 'mpirun', 'aprun' to use.
    Also deleted some unnecessary stuff - like the commands to copy to the nonexistent 'scratch' dir that always throws an error.
    
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@1006 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: 0528401e19dda66a6cfa9b7ab748309c23393f4d

--- tests/higher-order/dome/dome.py
+++ tests/higher-order/dome/dome.py
@@ -9,39 +9,35 @@
 
 # Slight alterations by SFP on 2-3-11 for Glimmer-CISM 2.0 relase
 
-import sys, os, glob, shutil, numpy
+# Parse options
+from optparse import OptionParser
+optparser = OptionParser()
+optparser.add_option("-c", "--config", dest="configfile", type='string', default='dome.config', help="Name of .config file to use to setup and run the dome test case", metavar="FILE")
+optparser.add_option('-m','--parallel',dest='parallel',type='int', help='Number of processors to run the model with: if specified then execute run in parallel', metavar="NUMPROCS")
+optparser.add_option('-e','--exec',dest='executable',default='./simple_glide',help='Set path to the CISM executable (defaults to "./simple_glide")')
+options, args = optparser.parse_args()
+
+import sys, os, numpy
 from netCDF import *
 from math import sqrt
 from ConfigParser import ConfigParser
 
-# Check to see if a config file was specified on the command line.
-# If not, dome.config is used.
-if len(sys.argv) > 1:
-  if sys.argv[1][0] == '-': # The filename can't begin with a hyphen
-    print '\nUsage:  python dome.py [FILE.CONFIG]\n'
-    sys.exit(0)
-  else:
-    configfile = sys.argv[1]
-else:
-  configfile = 'dome.config'
 
-# Check to see if #procs specified, relevant when running the code in parallel. 
-# If not, serial run (#procs==1) is performed. To run in parallel, the configure
-# file must be specifed, but the nu,ber of processors does not
-if len(sys.argv) > 2:
-    nprocs = sys.argv[2]
-else:
-  nprocs = '1'
 
+
+# =====================================
 # Create a netCDF file according to the information in the config file.
-parser = ConfigParser()
-parser.read(configfile)
-nx = int(parser.get('grid','ewn'))
-ny = int(parser.get('grid','nsn'))
-nz = int(parser.get('grid','upn'))
-dx = float(parser.get('grid','dew'))
-dy = float(parser.get('grid','dns'))
-filename = parser.get('CF input', 'name')
+try:
+    parser = ConfigParser()
+    parser.read(options.configfile)
+    nx = int(parser.get('grid','ewn'))
+    ny = int(parser.get('grid','nsn'))
+    nz = int(parser.get('grid','upn'))
+    dx = float(parser.get('grid','dew'))
+    dy = float(parser.get('grid','dns'))
+    filename = parser.get('CF input', 'name')
+except:
+    sys.exit('Error parsing ' + options.configfile)
 
 print 'Writing', filename
 try:
@@ -89,19 +85,30 @@ netCDFfile.createVariable('artm','f',('time','y1','x1'))[:] = artm
 
 netCDFfile.close()
 
-# Run Glimmer
-print 'Running Glimmer/CISM'
-if len(sys.argv) > 2:
-   #os.system('aprun -n'+nprocs+' ./simple_glide '+configfile+'')  # support for MPI runs is here
-   os.system('mpirun -np '+nprocs+' ./simple_glide '+configfile+'') 
-else:
-   os.system('echo '+configfile+' | ./simple_glide')
 
-# Clean up by moving extra files written by Glimmer to the "scratch" subdirectory
-# Look for files with extension "txt", "log", or "nc"
-for files in glob.glob('*.txt')+glob.glob('*.log'):
-# Delete any files already in scratch with these filenames 
-  if files in os.listdir('scratch'):
-    os.remove(os.path.join('scratch',files))
-# Move the new files to scratch
-  shutil.move(files,'scratch')
+# =====================================
+# Run CISM
+print 'Running CISM'
+print '============\n'
+if options.parallel == None:
+   # Perform a serial run
+   os.system(options.executable + ' ' + options.configfile)
+else:
+   # Perform a parallel run
+   if options.parallel <= 0:
+      sys.exit( 'Error: Number of processors specified for parallel run is <=0.' )
+   else:
+      # These calls to os.system will return the exit status: 0 for success (the command exists), some other integer for failure
+      if os.system('which openmpirun > /dev/null') == 0:
+         mpiexec = 'openmpirun -np '
+      elif os.system('which mpirun > /dev/null') == 0:
+         mpiexec = 'mpirun -np '
+      elif os.system('which aprun > /dev/null') == 0:
+         mpiexec = 'aprun -n '
+      else:
+         sys.exit('Unable to execute parallel run.  Please edit the script to use your MPI run command, or run the model manually with something like: mpirun -np 4 ./simple_glide dome.config')
+      runstring = mpiexec + str(options.parallel) + ' ' + options.executable + ' ' + options.configfile
+      print 'Executing parallel run with:  ' + runstring + '\n\n'
+      os.system(runstring)  # Here is where the parallel run is actually executed!
+
+
