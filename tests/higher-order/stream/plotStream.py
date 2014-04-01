3c513e6886bb18884613710fe08e7c595727f2c0
mhoffman <mhoffman@lanl.gov>
2014-04-01 02:10:30 +0000

    Substantial updates to the stream test case scripts.  Use 'python runStream.py --help' for usage details if you don't read any further of this log.
    * Removed stream.config and replaced it with stream.config.in.  Now runStream.py generates an unversioned stream.config.
    * The -s option to runStream.py is now used to specify how many cells you want the stream to be across.  The script then makes calculations for an appropriate size for the non-stream region of the domain, and then determines how big the entire domain should be, then uses that to setup stream.config and stream.nc.
    * The size of the non-stream area is no longer hard-coded to 3 cells.  For the Raymond case it is ~5 ice thicknesses.  For the Schoof case it is 0.5 to 1.5x the stream width(which is pretty wide!).
    * Added an alongFlowLength parameter to the run script so you don't have to model more of the domain than you really need for the periodic case.  (Or for the Dirichlet b.c. case, you may want to model a much longer section.)  You can get a square domain by setting it to -1.
    * Shifted the y coordinates to be symmetrical about 0.0 for convenience in plotting and making comparisons.
    * Discovered and removed some strange truncation errors in python that was inadvertently resulting in dx (and dy) not being exactly constant across the domain.
    * The Schoof case no longer has any 'large' mintauf values, but instead gets values entirely from the analytic function.  This should work in principle if the domain is wide enough.  In a simple test, I was unable to get it to converge, so with more testing we may decide to reinstate a few large mintauf rows if that helps with convergence.
    * Added a x-sect of mintauf to the plotting script's figure of the velocity.
    * Added a 2nd plot that shows along-flow profiles of the demeaned velocity for both the surface and the bed for both uvel and vvel.  Because these are demeaned and in the along-flow direction, they should all be flat lines, but they show periodic errors of ~1e-5 m/yr with default settings.  The plots include profiles along EVERY y-index - sometimes the central row is error-free, but other rows are not.
    * made the output file's xtype=double so the output file does not have truncated values relative to those in the model itself
    
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/trunk@1216 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: 93ecd8aefbfb94b3156ead071ef6c76d9aaed64e

--- tests/higher-order/stream/plotStream.py
+++ tests/higher-order/stream/plotStream.py
@@ -26,11 +26,19 @@ f = NetCDFFile(options.filename, 'r')
 y0 = f.variables['y0'][:]
 ny = y0.shape[0] + 1  # the actual ny dimension, not the size of y0
 dy = y0[1]-y0[0]
-y0_centered = y0 - (ny-1) * dy / 2.0  # use y coord that are symmetrical about 0
 
 uvel = f.variables['uvel'][0,:,:,:]  # get all levels at time 0
+vvel = f.variables['vvel'][0,:,:,:]  # get all levels at time 0
+mintauf = f.variables['tauf'][0,:,:]
+#btractx = f.variables['btractx'][0,:,:]
+#btracty = f.variables['btracty'][0,:,:]
 if netCDF_module == 'Scientific.IO.NetCDF':
    uvel = uvel * f.variables['uvel'].scale_factor
+   vvel = vvel * f.variables['vvel'].scale_factor
+   mintauf = mintauf * f.variables['tauf'].scale_factor
+#   btractx = btractx * f.variables['btractx'].scale_factor
+#   btracty = btracty * f.variables['btracty'].scale_factor
+#btract = (btractx**2 + btracty**2)**0.5
 
 x0 = f.variables['x0'][:]
 xpos = x0.shape[0]/2   # integer division on x-length to get the middle column of the domain
@@ -39,12 +47,12 @@ ypos = y0.shape[0]/2   # integer division on y-length to get the middle row of t
 
 # Calculate analytic velocity profile - the analytic functions are in runStream.py
 if analytic_solution == 'raymond':
-    uvel_analytic_profile = raymond_uvel(y0_centered)
+    uvel_analytic_profile = raymond_uvel(y0)
     analytic_name = 'Raymond analytic solution'
 elif analytic_solution == 'schoof':
-    uvel_analytic_profile = schoof_uvel(y0_centered)
+    uvel_analytic_profile = schoof_uvel(y0)
     # Some adjustments to the analytic profile - not entirely sure why these are needed.
-    ind = numpy.nonzero( numpy.absolute(y0_centered) >= W )
+    ind = numpy.nonzero( numpy.absolute(y0) >= streamHalfWidth )
     uvel_analytic_profile[ind] = uvel_analytic_profile.min()
     uvel_analytic_profile = uvel_analytic_profile - uvel_analytic_profile.min()
     analytic_name = 'Schoof analytic solution'
@@ -52,7 +60,7 @@ else:
     sys.exit("Error: Invalid value for 'analytic_solution'.")
 
 
-
+# ===================
 # Setup plot of uvel cross-section
 fig = plt.figure(1, facecolor='w', figsize=(12, 10), dpi=80)
 
@@ -66,16 +74,65 @@ plt.ylabel('along flow velocity (m/a)')
 plt.title(analytic_name +  ' at x=%.1f km'%(x0[xpos]/1000.0))
 plt.legend(loc='lower center')
 
-
 fig.add_subplot(2,1,2)
-#plt.plot(x0/1000.0, uvel_analytic_profile[ypos] * numpy.ones(x0.shape), '-or', label=analytic_name)
-plt.plot(x0/1000.0, uvel[ 0,ypos,:], '-xk', label='CISM surface')
-plt.plot(x0/1000.0, uvel[-1,ypos,:], '-^k', label='CISM basal')
+plt.plot(y0/1000.0, mintauf[:, xpos], '-bo', label='Yield stress')
+#plt.plot(y0/1000.0, btract[:, xpos], '-go', label='basal traction')
+plt.xlabel('distance across flow (km)')
+plt.ylabel('Yield stress (Pa)')
 
+# ===================
+# Plot other diagnostics
+fig = plt.figure(2, facecolor='w', figsize=(16, 10), dpi=80)
+
+fig.add_subplot(2,2,1)
+colors = plt.cm.get_cmap('jet',len(y0))
+for j in range(len(y0)):
+  plt.plot(x0/1000.0, uvel[ 0,j,:] - uvel[ 0,j,:].mean(), '.-', color=colors(j), label=str(j))
 plt.xlabel('distance along flow (km)')
-plt.ylabel('along flow velocity (m/a)')
-plt.title('Longit. x-sect. of model vel. at y=%.1f km (should be constant-valued)'%(y0[ypos]/1000.0))
-plt.legend(loc='lower center')
+plt.ylabel('surface ALONG flow velocity, demeaned (m/a)')
+plt.title('Longit. x-sect. of model vel. for all rows (should be constant-valued)')
+# kludgy code to get a colorbar for the lines
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.normalize(vmin=0, vmax=len(y0)))
+sm._A = []; 
+cb=plt.colorbar(sm)
+cb.set_label('y-index')
+
+fig.add_subplot(2,2,2)
+for j in range(len(y0)):
+  plt.plot(x0/1000.0, uvel[ -1,j,:] - uvel[ -1,j,:].mean(), '.-', color=colors(j), label=str(j))
+plt.xlabel('distance along flow (km)')
+plt.ylabel('basal ALONG flow velocity, demeaned (m/a)')
+plt.title('Longit. x-sect. of model vel. for all rows (should be constant-valued)')
+# kludgy code to get a colorbar for the lines
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.normalize(vmin=0, vmax=len(y0)))
+sm._A = []; 
+cb=plt.colorbar(sm)
+cb.set_label('y-index')
+
+fig.add_subplot(2,2,3)
+for j in range(len(y0)):
+  plt.plot(x0/1000.0, vvel[ 0,j,:] - vvel[ 0,j,:].mean(), '.-', color=colors(j), label=str(j))
+plt.xlabel('distance along flow (km)')
+plt.ylabel('surface ACROSS flow velocity, demeaned (m/a)')
+plt.title('Longit. x-sect. of model vel. for all rows (should be constant-valued)')
+# kludgy code to get a colorbar for the lines
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.normalize(vmin=0, vmax=len(y0)))
+sm._A = []
+cb=plt.colorbar(sm)
+cb.set_label('y-index')
+
+fig.add_subplot(2,2,4)
+for j in range(len(y0)):
+  plt.plot(x0/1000.0, vvel[-1,j,:] - vvel[-1,j,:].mean(), '.-', color=colors(j), label=str(j))
+plt.xlabel('distance along flow (km)')
+plt.ylabel('basal ACROSS flow velocity, demeaned (m/a)')
+plt.title('Longit. x-sect. of model vel. for all rows (should be constant-valued)')
+# kludgy code to get a colorbar for the lines
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.normalize(vmin=0, vmax=len(y0)))
+sm._A = []
+cb=plt.colorbar(sm)
+cb.set_label('y-index')
+
 
 plt.draw()
 plt.show()
--- tests/higher-order/stream/runStream.py
+++ tests/higher-order/stream/runStream.py
@@ -3,33 +3,32 @@
 # File to generate an input netcdf file readable by CISM for the stream test case and run the model.
 
 # Import some modules here
-import sys, os, numpy
+import numpy
 
 # =====================================
 # parameter values to use in this script and the post-processing plotting script.
 # =====================================
 
+# ==========================================================================
+# Parameters you might want to modify
+# ==========================================================================
 analytic_solution = 'raymond'  # can be 'raymond' or 'schoof'
-kinflag = 1    # 1=apply kinematic bc (analytic soln) at up/downstream ends, 0=the run will be doubly periodic (preferred)
+kinflag = 0    # 1=apply kinematic bc (analytic soln) at up/downstream ends, 0=the run will be doubly periodic (preferred)
+
+# Domain parameters
+streamHalfWidth = 25000.0   # ice stream half-width, in m - used for both Raymond & Schoof formulations
+alongFlowLength = 50000.0   # the desired along-flow length of the domain, in m; set to -1 to get a square domain
+H = 1000.0       # ice thickness
+dsdx = -1.0e-3   # bed (and surface) slope in the x-direction (y-direction is flat)
 
 # Physical parameters
 rho = 910.0   # ice density kg/m3
 g = -9.81     # gravity m/s2
 n = 3         # flow law exponent
 A = 1e-16     # flow rate factor in Pa^-3 yr^-1
-B = A**(-1.0/n)
+# ==========================================================================
 
-# Domain parameters
-H = 1000.0       # ice thickness
-dsdx = -1.0e-3   # bed (and surface) slope in the x-direction (y-direction is flat)
-# ice stream width
-w = 50.0e3
-W = w / 2
-taud = rho * g * H * dsdx
 
-# Schoof solution parameters
-m = 1.55  # Schoof exponent
-L = 1.4e4  # Schoof yield stress width parameter  ## TODO Steve is this correct?
 
 # ======================================
 # -- Functions for analytic solutions --
@@ -37,21 +36,29 @@ L = 1.4e4  # Schoof yield stress width parameter  ## TODO Steve is this correct?
 
 # Raymond yield stress
 def raymond_tau(yy):
-  return 5.2e3*numpy.ones(yy.shape)
+  tau0 = 5.2e3*numpy.ones(yy.shape)         # set the stream value everywhere
+  tau0[numpy.absolute(yy)>streamHalfWidth] = 0.7e5        # set a very large value  outside the stream
+  return tau0
+
+# Raymond velocity solution
+def raymond_uvel(yy):
+  tau0r = raymond_tau(yy)
+  tau0r[tau0r>taud] = taud
+  ur = 2.0 * A / (n+1.0) * ( (taud - tau0r)/H )**n * ( streamHalfWidth**(n+1) - numpy.absolute(yy)**(n+1) )
+  ur[ur<0.0] = 0.0
+  return ur
+
+# Schoof solution parameters
+m = 1.55  # Schoof exponent
+L = streamHalfWidth
 
 # Schoof yield stress distribution
 def schoof_tau(yy):
   return taud * numpy.absolute( yy / L )**m
 
-# Raymond velocity solution
-def raymond_uvel(yy):
-  tau0r = raymond_tau(yy)
-  ur = 2.0 * A / (n+1.0) * ( (taud - tau0r)/H )**n * ( W**(n+1) - yy**(n+1) )
-  ur[ur<0.0] = 0.0
-  return ur
-
 # Schoof velocity solution
 def schoof_uvel(yy):
+  B = A**(-1.0/n)
   us = -2.0 * taud**3 * L**4 / (B**3 * H**3) * ( ((yy/L)**4 - (m+1.0)**(4.0/m))/4.0 - 3.0*( numpy.absolute(yy/L)**(m+4.0) \
     - (m+1.0)**(1.0+4.0/m) )/((m+1.0)*(m+4.0)) + 3.0*( numpy.absolute(yy/L)**(2.0*m+4.0) - (m-1.0)**(2.0+4.0/m) )/((m+1.0)**2*(2.0*m+4.0)) \
     - ( numpy.absolute(yy/L)**(3.0*m+4.0) - (m+1.0)**(3.0+4.0/m) )/ ( (m+1.0)**3.0*(3.0*m+4.0)) )
@@ -59,42 +66,92 @@ def schoof_uvel(yy):
 ##sscale = 2*taud^3*L^4/(B^3*H^3);  # this was unused in .m script, but copying it over anyway
 
 
+# ======================================
+# -- Values derived from things above --
+# ======================================
+taud = rho * g * H * dsdx  # Driving stress
+# Calculate a good size for the size of the domain outside of the stream (in m)
+if analytic_solution == 'raymond':
+  strongWidth = 5.0 * H  # 5 ice thicknesses should get us beyond the zone of lateral stress transfer.  Adjust as needed
+elif analytic_solution == 'schoof':
+  # These values are very roughly approximated from Figure 3 of Schoof (2006).  Adjust as needed
+  if m<10.0:
+    strongWidth = 1.5 * L
+  else:
+    strongWidth = 0.5 * L
+# Calculating the actual domain size will happen later after the config file is read
+
+
+
+
 # =====================================
 # The actual script to run
 # =====================================
 if __name__ == '__main__':
+
+    # =====================================
+    # Initialization stuff
+
     # Parse options
     from optparse import OptionParser
     optparser = OptionParser()
-    optparser.add_option("-c", "--config", dest="configfile", type='string', default='stream.config', help="Name of .config file to use to setup and run the test case", metavar="FILE")
+    optparser.add_option("-c", "--config", dest="configfile", type='string', default='stream.config.in', help="Name of .config file to use to setup and run the test case", metavar="FILE")
     optparser.add_option('-m','--parallel',dest='parallel',type='int', help='Number of processors to run the model with: if specified then execute run in parallel', metavar="NUMPROCS")
-    optparser.add_option('-e','--exec',dest='executable',default='./simple_glide',help='Set path to the CISM executable (defaults to "./simple_glide")')
+    optparser.add_option('-e','--exec',dest='executable',default='./simple_glide',help='Set path to the CISM executable')
+    optparser.add_option('-s','--stream-size',dest='stream_grid_size',default=25,type='int',help='Number of cells to use to model the ice stream portion of the domain.')
+    optparser.add_option('-v','--vert-grid-size',dest='vertical_grid_size',type='int',help='Number of vertical layers to use (upn)')
+
     for option in optparser.option_list:
         if option.default != ("NO", "DEFAULT"):
             option.help += (" " if option.help else "") + "[default: %default]"
     options, args = optparser.parse_args()
 
-
+    # Other modules needed by the main script
     from netCDF import *
-    import ConfigParser
-
+    import ConfigParser, sys, os
 
     # =====================================
     # Create a netCDF file according to the information in the config file.
     try:
         configParser = ConfigParser.SafeConfigParser()
         configParser.read(options.configfile)
-        nx = int(configParser.get('grid','ewn'))
-        ny = int(configParser.get('grid','nsn'))
         nz = int(configParser.get('grid','upn'))
-        dx = float(configParser.get('grid','dew'))
-        dy = float(configParser.get('grid','dns'))
         filename = configParser.get('CF input', 'name')
     except:
         sys.exit('Error parsing ' + options.configfile)
 
-    if (ny % 2) != 0:
-      sys.exit("Error: 'nsn' in the config file must be divisible by 2")
+
+    # =====================================
+    # Figure out domain size information
+    nStream = int(options.stream_grid_size)
+    dy = streamHalfWidth * 2.0 / float(nStream)
+    nStrongStrip = int(round(strongWidth / dy))  # Figure out the number of cells we need to add to get as close the the desired width of the strong region as possible (note: may want to use ceil() instead of round() here)
+    ny = nStream + 2 * nStrongStrip + 1  # the +1 is to convert from y0 to y1
+    dx = dy  # always want this
+    print 'Number of cells for stream (N-S):', nStream
+    print 'Number of cells for entire domain (N-S):', ny
+    print 'dy=dx=',dy
+    print 'Domain N-S (across-flow) width (m):', ny*dy
+    if alongFlowLength < 0:
+      nx = ny  # square domain
+    else:
+      nx = int(round(alongFlowLength / dx))
+    print 'Domain E-W (along-flow) width (m):', nx*dx
+
+    # Now set the values in our internal config object to get written out later.
+    configParser.set('grid','ewn',str(nx))
+    configParser.set('grid','nsn',str(ny))
+    configParser.set('grid','dew',str(dx))
+    configParser.set('grid','dns',str(dy))
+
+    if options.vertical_grid_size:
+      nz = int(options.vertical_grid_size)
+      configParser.set('grid','upn',str(nz))
+      print 'Number of vertical layers has been set to:', nz
+
+    # Check domain sizes for usefulness
+    if (nStream % 2) == 0 and analytic_solution == 'schoof':
+      print "Warning: For the Schoof version, you might want the number of cells in the stream to be an odd number."
 
     print 'Writing', filename
     try:
@@ -109,16 +166,25 @@ if __name__ == '__main__':
     netCDFfile.createDimension('x0',nx-1) # staggered grid 
     netCDFfile.createDimension('y0',ny-1)
 
-    x1 = dx*numpy.arange(nx,dtype='float32')
-    y1 = dx*numpy.arange(ny,dtype='float32')
-    x0 = dx/2 + x1[:-1] # staggered grid
-    y0 = dy/2 + y1[:-1]
+    x1 = dx*numpy.arange(nx,dtype='float64')
+    y1 = dy*numpy.arange(ny,dtype='float64') - dy*float(ny-1)/2.0  # make the y-coordinates centered about 0
+    x0 = dx/2.0 + x1[:-1] # staggered grid
+    y0 = dy/2.0 + y1[:-1]
 
+    # Make sure the edge of the stream lands on the edge of grid cells on the y0 grid - this is the cell centers on the y1 grid
+    if not streamHalfWidth in y1:
+      sys.exit('Error: the stream edge does not land on an edge of the y0 grid so the stream will not be resolved.  Adjust your domain size and/or stream size and/or horizontal resolution.  \nstream half width='+str(streamHalfWidth)+'\ny0 grid has edges at:\n '+str(y1[:]))
+
+    # Make sure we have at least two non-stream rows on each side
+    if (numpy.absolute(y0[:])>streamHalfWidth).sum() < 4:
+      sys.exit('Error: there are less than two non-stream rows on each side of the stream.  Adjust your domain size and/or stream size and/or horizontal resolution.  \nstream half width='+str(streamHalfWidth)+'\ny0 grid has cell centers at:\n '+str(y0[:]))
+
+    # If all is good, continue.
     netCDFfile.createVariable('time','f',('time',))[:] = [0]
-    netCDFfile.createVariable('x1','f',('x1',))[:] = x1
-    netCDFfile.createVariable('y1','f',('y1',))[:] = y1
-    netCDFfile.createVariable('x0','f',('x0',))[:] = x0
-    netCDFfile.createVariable('y0','f',('y0',))[:] = y0
+    netCDFfile.createVariable('x1','f',('x1',))[:] = numpy.float32(x1)
+    netCDFfile.createVariable('y1','f',('y1',))[:] = numpy.float32(y1)
+    netCDFfile.createVariable('x0','f',('x0',))[:] = numpy.float32(x0)
+    netCDFfile.createVariable('y0','f',('y0',))[:] = numpy.float32(y0)
 
     # Calculate values for the required variables.
     thk  = numpy.zeros([1,ny,nx],dtype='float32')
@@ -132,21 +198,17 @@ if __name__ == '__main__':
     for j in range(ny):
       topg[0,j,:] = 1000.0 + dsdx * x1[:]   # sloped bed.  add 1000.0 to stay well above sea level
 
-    y0_centered = y0 - (ny-1) * dy / 2.0  # use y coord that are symmetrical about 0
-
     if analytic_solution == 'raymond':
-        tau0Profile = raymond_tau(y0_centered)
-        uvelProfile = raymond_uvel(y0_centered)
+        tau0Profile = raymond_tau(y0)
+        uvelProfile = raymond_uvel(y0)
     elif analytic_solution == 'schoof':
-        tau0Profile = schoof_tau(y0_centered)
-        uvelProfile = schoof_uvel(y0_centered)
+        tau0Profile = schoof_tau(y0)
+        uvelProfile = schoof_uvel(y0)
     else:
         sys.exit("Error: Invalid value for 'analytic_solution'.")
     for i in range(nx-1):
       tauf[0,:,i] = tau0Profile
-    # put no slip along lateral boundaries - 3 cells thick along north and south
-    tauf[0,0:3,:] = 0.7e5
-    tauf[0,-3:,:] = 0.7e5
+
 
     # =======================================
     # Save the required variables to the netCDF file.
@@ -188,7 +250,10 @@ if __name__ == '__main__':
     # Update periodic offset in config file
     offset = -dsdx * dx * nx
     configParser.set('parameters', 'periodic_offset_ew', str(offset))
-    configFile = open(options.configfile,'w')
+
+    # Save periodic offset and possibly nsn, ewn, dns, dew, upn to the config file
+    runConfigFile = 'stream.config'
+    configFile = open(runConfigFile,'w')
     configParser.write(configFile)
     configFile.close()
 
@@ -196,11 +261,12 @@ if __name__ == '__main__':
 
     # =====================================
     # Run CISM
+
     print 'Running CISM'
     print '============\n'
     if options.parallel == None:
        # Perform a serial run
-       os.system(options.executable + ' ' + options.configfile)
+       os.system(options.executable + ' ' + runConfigFile)
     else:
        # Perform a parallel run
        if options.parallel <= 0:
@@ -215,7 +281,7 @@ if __name__ == '__main__':
              mpiexec = 'aprun -n '
           else:
              sys.exit('Unable to execute parallel run.  Please edit the script to use your MPI run command, or run the model manually with something like: mpirun -np 4 ./simple_glide stream.config')
-          runstring = mpiexec + str(options.parallel) + ' ' + options.executable + ' ' + options.configfile
+          runstring = mpiexec + str(options.parallel) + ' ' + options.executable + ' ' + runConfigFile
           print 'Executing parallel run with:  ' + runstring + '\n\n'
           os.system(runstring)  # Here is where the parallel run is actually executed!
 
