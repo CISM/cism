f9d180e33a480c6220613548de7b50c0b3af0cb6
sacks <sacks@ucar.edu>
2014-10-09 01:59:24 +0000

    branch commit: fix (I think) distributed_put_var_real8_2d
    git-svn-id: http://oceans11.lanl.gov/svn/PISCEES/branches/fix_parallel_mpi_for_glide@1804 42a2fd4b-9c52-44de-a8a9-1772c67210cd
    
    Former-commit-id: 29462ff74b3157094887454875ce9043414bdede

--- libglimmer/parallel_mpi.F90
+++ libglimmer/parallel_mpi.F90
@@ -2127,7 +2127,8 @@ contains
     integer,dimension(:) :: start
     real(8),dimension(:,:) :: values
 
-    integer :: ew,i,ierror,ns
+    type(bounds_info_type) :: bounds_info
+    integer :: i,ierror
     integer,dimension(4) :: mybounds
     integer,dimension(:),allocatable :: displs,recvcounts
     integer,dimension(:,:),allocatable :: bounds
@@ -2136,19 +2137,12 @@ contains
 
     ! begin
 
-    if (size(values,1)==local_ewn) then
-       ew = global_ewn
-       ns = global_nsn
-    else if (size(values,1)==local_ewn-1) then
-       ew = global_ewn-1
-       ns = global_nsn-1
-    else
-       call parallel_stop(__FILE__,__LINE__)
-    end if
-    mybounds(1) = ewlb+lhalo
-    mybounds(2) = ewub-uhalo
-    mybounds(3) = nslb+lhalo
-    mybounds(4) = nsub-uhalo
+    bounds_info = get_bounds_info(size(values,1))
+
+    mybounds(1) = bounds_info%mybounds_ew_lb
+    mybounds(2) = bounds_info%mybounds_ew_ub
+    mybounds(3) = bounds_info%mybounds_ns_lb
+    mybounds(4) = bounds_info%mybounds_ns_ub
     if (main_task) then
        allocate(bounds(4,tasks))
     else
@@ -2174,7 +2168,8 @@ contains
        allocate(recvbuf(1))
     end if
     allocate(sendbuf(mybounds(1):mybounds(2),mybounds(3):mybounds(4)))
-    sendbuf(:,:) = values(1+lhalo:local_ewn-uhalo,1+lhalo:local_nsn-uhalo)
+    sendbuf(:,:) = values(bounds_info%ilo:bounds_info%ihi, &
+                          bounds_info%jlo:bounds_info%jhi)
     call fc_gatherv_real8(sendbuf,size(sendbuf),mpi_real8,&
        recvbuf,recvcounts,displs,mpi_real8,main_rank,comm)
     if (main_task) then
@@ -2184,7 +2179,7 @@ contains
                (/bounds(2,i)-bounds(1,i)+1,bounds(4,i)-bounds(3,i)+1/))
        end do
        distributed_put_var_real8_2d = nf90_put_var(ncid,varid,&
-            global_values(1:ew,1:ns),start)
+            global_values(1:bounds_info%global_ewn, 1:bounds_info%global_nsn),start)
     end if
     call broadcast(distributed_put_var_real8_2d)
     !automatic deallocation
