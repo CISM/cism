47fff1b15ac0477dd050b1b226a2e0e581edec0c
Matthew Hoffman <mhoffman@lanl.gov>
2014-12-08 15:57:00 -0700

    Update beta read check to be <0 instead of <=0
    Commit 6bb8cbbc introduced a check of the values of beta to determine if
    a beta field had been read from file if using HO_BABC_EXTERNAL_BETA.
    The check was for (maxval(beta) <= 0.d0).  However some of our test cases
    have beta=0 everywhere (ice shelf test case), so this commit relaxes
    that check to (maxval(beta) < 0.d0) instead.

--- libglissade/glissade_basal_traction.F90
+++ libglissade/glissade_basal_traction.F90
@@ -261,8 +261,8 @@ contains
 
        ! beta is initialized to a negative value; we can use that fact to check whether
        ! it has been read correctly from the file
-       if (maxval(beta) <= 0.d0) then
-          call write_log('ERROR: Trying to use HO_BABC_EXTERNAL_BETA, but all beta values are <= 0,')
+       if (maxval(beta) < 0.d0) then
+          call write_log('ERROR: Trying to use HO_BABC_EXTERNAL_BETA, but all beta values are < 0,')
           call write_log('which implies that beta could not be read from the input file.')
           call write_log('Make sure that beta is in the cism input file,')
           call write_log('or change which_ho_babc to a different option.')
